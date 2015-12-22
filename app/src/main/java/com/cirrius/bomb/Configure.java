aa2cde94485f1567687c2b8c75c58310925cdef1
gb17 <gaurav.b@cirrius.com>
2015-12-22 18:12:45 +0530

    Added TextView

--- app/src/main/java/com/cirrius/bomb/Configure.java
+++ app/src/main/java/com/cirrius/bomb/Configure.java
@@ -30,6 +30,10 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 
 /**
@@ -54,7 +58,49 @@ public class Configure extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DBHandler handler = new DBHandler(this);
+        handler.getWritableDatabase();
         setContentView(R.layout.configure);
+        try {
+            JSONObject student1 = new JSONObject();
+            try {
+                student1.put("id", "3");
+                student1.put("name", "NAME OF STUDENT");
+                student1.put("year", "3rd");
+                student1.put("curriculum", "Arts");
+                student1.put("birthday", "5/5/1993");
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            JSONObject student2 = new JSONObject();
+            try {
+                student2.put("id", "2");
+                student2.put("name", "NAME OF STUDENT2");
+                student2.put("year", "4rd");
+                student2.put("curriculum", "scicence");
+                student2.put("birthday", "5/5/1993");
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+
+            JSONArray jsonArray = new JSONArray();
+
+            jsonArray.put(student1);
+            jsonArray.put(student2);
+
+            JSONObject studentsObj = new JSONObject();
+            studentsObj.put("Students", jsonArray);
+            String jsonStr = studentsObj.toString();
+            System.out.println("jsonString: "+jsonStr);
+        } catch (Exception e) {
+
+        }
         fixed_column = (TableLayout) findViewById(R.id.fixed_column);
         scrollable_part = (TableLayout) findViewById(R.id.scrollable_part);
 
--- app/src/main/java/com/cirrius/bomb/DBHandler.java
+++ app/src/main/java/com/cirrius/bomb/DBHandler.java
@@ -1,7 +1,35 @@
 package com.cirrius.bomb;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
 /**
  * Created by admin on 12/22/15.
  */
-public class DBHandler  {
+public class DBHandler extends SQLiteOpenHelper {
+
+    public static String DB_NAME = "BOMB";
+    public static String TABLE_NAME = "TBGRID";
+    public static int DB_VERSION = 1;
+    Context context;
+
+    public DBHandler(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+        Toast.makeText(context, "M here", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "CREATE TABLE " + TABLE_NAME + " (COL0 int auto increment, COL1 text, COL2 text, COL3 text, COL4 text, COL5 text, COL6 text, COL7 text, COL8 text, COL9 text, COL10 text, COL11 text, COL12 text, COL13 text, COL14 text, COL15 text  )";
+        Toast.makeText(context, "M here2", Toast.LENGTH_LONG).show();
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
